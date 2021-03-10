3d27299540c9660ec3ca68ed0f8cc5ba0b9397fd
Jersen Meim <51324287+Jersenmeim@users.noreply.github.com>
2021-03-10 22:15:26 +0800

    Week 10 aDDED

--- js/main.js
+++ js/main.js
@@ -1,77 +1,81 @@
 const week = [{
     title: "Week 1",
-    link : "weekone.html",
+    link: "weekone.html",
   },
   {
     title: "Week 2",
-    link : "weektwo.html",
+    link: "weektwo.html",
   },
 
   {
     title: "Week 3",
-    link : "weekthree.html",
+    link: "weekthree.html",
   },
 
   {
     title: "Week 4",
-    link : "weekfour.html",
+    link: "weekfour.html",
   },
 
   {
     title: "Week 5",
-    link : "weekfive.html",
+    link: "weekfive.html",
   },
 
   {
     title: "Todo App",
-    link : "./TodoApp/index.html",
+    link: "./TodoApp/index.html",
   },
 
   {
     title: "Week 7",
-    link : "weekseven.html",
+    link: "weekseven.html",
   },
-  
+
   {
     title: "Week 8",
-    link : "weekeight.html",
+    link: "weekeight.html",
   },
 
   {
     title: "Week 9",
-    link : "weeknine.html",
+    link: "weeknine.html",
+  },
+  {
+    title: "Week 10",
+    link: "weekten.html",
   }
-  
+
 ];
 
 week.forEach(week => {
-    
-    
-    let div1 = document.createElement ('li');
-    div1.setAttribute('class', 'section');
-  
-    let a = document.createElement('a');
-    a.setAttribute ('href', week.link);
 
-    let h4 = document.createElement('h4');
-    h4.setAttribute ('id', 'link');
 
-   
-    let p1 = document.createElement('p');
+  let div1 = document.createElement('li');
+  div1.setAttribute('class', 'section');
 
-    let a2 = document.createElement('a');
-    a2.setAttribute ('href', week.link);
+  let a = document.createElement('a');
+  a.setAttribute('href', week.link);
 
-    h4.textContent = week.title;
+  let h4 = document.createElement('h4');
+  h4.setAttribute('id', 'link');
 
-    
-    
-    p1.appendChild(a2);
-    a.appendChild(h4);
-    div1.appendChild(a);
-   
-    div1.appendChild(p1);
 
-    document.querySelector('ul.body').appendChild(div1);
-    
-    });
+  let p1 = document.createElement('p');
+
+  let a2 = document.createElement('a');
+  a2.setAttribute('href', week.link);
+
+  h4.textContent = week.title;
+
+
+
+  p1.appendChild(a2);
+  a.appendChild(h4);
+  div1.appendChild(a);
+
+  div1.appendChild(p1);
+
+  document.querySelector('ul.body').appendChild(div1);
+
+});
